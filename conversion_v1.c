a6ef6b3dfd1c9fb9378ffff2044635e66cd61957
Caelum Dudek <caelumdudek25@gmail.com>
2024-05-24 15:12:00 -0700

    Fixed bounding?

--- conversion_v1.c
+++ conversion_v1.c
@@ -1,20 +1,29 @@
 #include <stdio.h>
-#include <stdint.h>
 #include <string.h>
 #include <stdlib.h>
+
+float in_bounds(float value){
+    if (value>255){
+        return 255;
+    } if (value < 0 ){
+        return 0;
+    }
+    return value
+}
 
 void rgb_to_ycc(float R, float G, float B){
     float Y = (0.299 * R) + (0.587 * G) + (0.114 * B);
     float Cb = 128 - (0.168736 * R) - (0.331264 * G) + (0.5 * B);
     float Cr = 128 + (0.5 * R) - (0.418688 * G) - (0.081312 * B);
-    printf("YCC %.0f %.0f %.0f", Y, Cb, Cr);
+
+    printf("YCC %.5f %.5f %.5f", in_bounds(Y), in_bounds(Cb), in_bounds(Cr));
 }
 
 void ycc_to_rgb(float Y, float Cr, float Cb){
     float R = Y + (1.402) * (Cr-128);
     float G = Y - 0.344136 * (Cb - 128)-0.714136 * (Cr-128);
     float B = Y + 1.772 * (Cb - 128);
-    printf("RGB %.0f %.0f %.0f", R, G, B);
+    printf("RGB %.5f %.5f %.5f", in_bounds(R), in_bounds(G), in_bounds(B));
 }
 
 int main(int argc, char *argv[])
