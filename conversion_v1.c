89dc915d732405ee4f06ba0bcf7b324d86e41301
ChloeZacharias <chloe.m.zacharias@gmail.com>
2024-05-24 15:54:14 -0700

    Fixed test cases for RGB - YCC

--- conversion_v1.c
+++ conversion_v1.c
@@ -2,16 +2,21 @@
 #include <string.h>
 #include <stdlib.h>
 
-float in_bounds(float value){
-    if (value>255){
+float in_bounds(float value)
+{
+    if (value > 255)
+    {
         return 255;
-    } if (value < 0 ){
+    }
+    if (value < 0)
+    {
         return 0;
     }
     return value;
 }
 
-void rgb_to_ycc(float R, float G, float B){
+void rgb_to_ycc(float R, float G, float B)
+{
     float Y = (0.299 * R) + (0.587 * G) + (0.114 * B);
     float Cb = 128 - (0.168736 * R) - (0.331264 * G) + (0.5 * B);
     float Cr = 128 + (0.5 * R) - (0.418688 * G) - (0.081312 * B);
@@ -19,22 +24,26 @@ void rgb_to_ycc(float R, float G, float B){
     printf("YCC %.0f %.0f %.0f", in_bounds(Y), in_bounds(Cb), in_bounds(Cr));
 }
 
-void ycc_to_rgb(float Y, float Cr, float Cb){
-    float R = Y + (1.402) * (Cr-128);
-    float G = Y - 0.344136 * (Cb - 128)-0.714136 * (Cr-128);
-    float B = Y + 1.772 * (Cb - 128);
+void ycc_to_rgb(float Y, float Cb, float Cr)
+{
+    float R = Y + ((1.402) * (Cr - 128));
+    float G = Y - (0.344136 * (Cb - 128)) - (0.714136 * (Cr - 128));
+    float B = Y + (1.772 * (Cb - 128));
     printf("RGB %.0f %.0f %.0f", in_bounds(R), in_bounds(G), in_bounds(B));
 }
 
 int main(int argc, char *argv[])
 {
-    char* conversion_type = argv[1];
-    if (strcmp(conversion_type, "RGB")==0){
+    char *conversion_type = argv[1];
+    if (strcmp(conversion_type, "RGB") == 0)
+    {
         float R = atof(argv[2]);
         float G = atof(argv[3]);
         float B = atof(argv[4]);
         rgb_to_ycc(R, G, B);
-    } else {
+    }
+    else
+    {
         float Y = atof(argv[2]);
         float Cr = atof(argv[3]);
         float Cb = atof(argv[4]);
