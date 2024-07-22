e797f9572fc743620f09c3aee4cc37da5f14b9a6
Caelum Dudek <caelumdudek25@gmail.com>
2024-07-22 08:47:40 -0700

    Done Neon RGB to YCC

--- Color_Space_Conversion/LESSON_103_CSC/C/test.py
+++ Color_Space_Conversion/LESSON_103_CSC/C/test.py
@@ -1,54 +1,66 @@
 import subprocess
+import sys
 import time
 import os
 from datetime import datetime
 import re
 
-# Define the C source file and the output executable file
-main_file = "CSC_main.c"
-extra_files = ["CSC_RGB_to_YCC_01.c", "CSC_YCC_to_RGB_01.c"]
 
-output_file = "CSC_main"
+def main(output=True):
+    # Define the C source file and the output executable file
+    main_file = "CSC_main.c"
+    extra_files = ["CSC_RGB_to_YCC_01.c", "CSC_YCC_to_RGB_01.c"]
 
-command_compile = f"gcc -O3 -o {output_file} {main_file} {' '.join(extra_files)} -mfpu=neon"
+    output_file = "CSC_main"
 
-# Compile the C program
-print(f"Compiling Program...\n{command_compile}")
-os.system(command_compile)
+    command_compile = f"gcc -O3 -o {output_file} {main_file} {' '.join(extra_files)} -mfpu=neon"
 
-# Check if the compilation was successful
-if os.path.isfile(output_file):
-    print(f"Compilation successful, executable '{output_file}' created.")
-else:
-    print("Compilation failed.")
-    exit(1)
+    # Compile the C program
+    print(f"Compiling Program...\n{command_compile}")
+    os.system(command_compile)
 
-command_run = f"./{output_file}"
-start_time = time.time()
-os.system(command_run)
+    # Check if the compilation was successful
+    if os.path.isfile(output_file):
+        print(f"Compilation successful, executable '{output_file}' created.")
+    else:
+        print("Compilation failed.")
+        exit(1)
 
-result_time = time.time() - start_time
+    command_run = f"./{output_file}"
+    start_time = time.time()
+    os.system(command_run)
 
-print(f"--- Program ran in {result_time} seconds ---")
+    result_time = time.time() - start_time
 
-print("=" * 20)
+    print(f"--- Program ran in {result_time} seconds ---")
 
-current_datetime = datetime.now().strftime("%Y-%m-%d %H-%M-%S")
+    print("=" * 20)
 
-command_cachegrind = f"valgrind --tool=cachegrind --cachegrind-out-file=cachegrind.out {command_run}"
+    current_datetime = datetime.now().strftime("%Y-%m-%d %H-%M-%S")
 
-result_cachegrind = subprocess.run(command_cachegrind, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE,
-                                   text=True)
+    command_cachegrind = f"valgrind --tool=cachegrind --cachegrind-out-file=cachegrind.out {command_run}"
 
-command_cachegrind_annotate = f"cg_annotate cachegrind.out"
+    result_cachegrind = subprocess.run(command_cachegrind, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE,
+                                       text=True)
 
-result_cachegrind_annotate = subprocess.run(command_cachegrind_annotate, shell=True, stdout=subprocess.PIPE,
-                                            stderr=subprocess.PIPE,
-                                            text=True)
+    if result_cachegrind.returncode != 0:
 
-output = f"""Log({current_datetime}):\nRuntime: {result_time}\n{result_cachegrind_annotate.stdout}\nCachegrind Result: \n{result_cachegrind.stdout}"""
+        print(f"Running the program resulted in an error: \n{result_cachegrind.stdout}")
+        exit(1)
 
-with open(f"./Logs/LOG-{current_datetime}.log", "w") as f:
-    f.write(output)
+    command_cachegrind_annotate = f"cg_annotate cachegrind.out"
 
-print(f"Finished, log written: Logs/LOG-{current_datetime}.log")
+    result_cachegrind_annotate = subprocess.run(command_cachegrind_annotate, shell=True, stdout=subprocess.PIPE,
+                                                stderr=subprocess.PIPE,
+                                                text=True)
+
+    output = f"""Log({current_datetime}):\nRuntime: {result_time}\n{result_cachegrind_annotate.stdout}\nCachegrind Result: \n{result_cachegrind.stdout}"""
+
+    if output:
+        with open(f"./Logs/LOG-{current_datetime}.log", "w") as f:
+            f.write(output)
+        print(f"Finished, log written: Logs/LOG-{current_datetime}.log")
+
+
+if __name__ == '__main__':
+    main(sys.argv[0] == "out")
